a0709a714e2f1b34e5a584bf9663e00b3f586e10
Nwsheats <nwsheats@gmail.com>
2022-09-25 18:28:29 -0500

    worked on local storage and button logic

--- script2.js
+++ script2.js
@@ -1,15 +1,28 @@
 let score = localStorage.getItem("Final Score");
 let scoreCounter = document.getElementById("score-counter");
 let scoreBtn = document.getElementById("score-btn");
+let clearScore = document.getElementById("clear-score");
 let startOver = document.getElementById("start-over-btn");
 let input = document.getElementById("input-field");
 let scoreList = $('#score-field');
 
-scoreCounter.textContent = "Your final score is: " + score;
+addEventListener('load', (event) => {
+    if (score == null) {
+        scoreCounter.textContent = "Your final score is: ";
+    } else {
+        scoreCounter.textContent = "Your final score is: " + score;
+    }});
+
+
+addEventListener('load', (event) => {
+    let storedName = localStorage.getItem("Name");
+    let storedScore = localStorage.getItem("Final Score");
+    scoreList.append('<li>' + storedName + ": " + storedScore + '</li>');
+});
 
 let scoreFormEl = $('#score-form');
+let clearFormEl = $('#clear-score');
 
-// create function to handle form submission
 function handleLogScore(event) {
   event.preventDefault();
   let playerName = $('input[name="input-field"]').val();
@@ -17,14 +30,23 @@ function handleLogScore(event) {
     alert('No name entered!');
     return;
   }
+  scoreBtn.disabled = true
   scoreList.append('<li>' + playerName + ": " + score + '</li>');
   $('input[name="input-field"]').val('');
   localStorage.setItem("Name", playerName)
   localStorage.setItem("Final Score", score)
+  scoreCounter.textContent = "";
 }
+
+
+function clearHighScore() {
+    localStorage.clear();
+}
+
+
 
 //make an object for localStorage with playerName and score
 
-scoreFormEl.on('submit', handleLogScore)
-
+scoreFormEl.on('submit', handleLogScore) 
+clearFormEl.on('click', clearHighScore)
 
