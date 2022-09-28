698046d9af457c422548b8d638d37c620098daaa
Nwsheats <nwsheats@gmail.com>
2022-09-27 22:39:08 -0500

    added additional functionality to local storage in second js document.

--- script2.js
+++ script2.js
@@ -7,11 +7,8 @@ let clearScore = document.getElementById("clear-score");
 let input = document.getElementById("input-field");
 let scoreList = $('#score-field');
 
-// these empty arrays were an attempt at something more difficult that I couldn't figure out
-// but they exist to have values pushed to them so that they can be sent to Local Storage and back using JSON stringify and parsify.
+let scoreArray = []
 
-let tempPName = [];
-let tempFScore = [];
 
 // this is load Event Listener designed to grab the score from Local Storage. If the score is null, it does not populate a value.
 // if the score is not null, it shows the number.
@@ -22,23 +19,25 @@ addEventListener('load', (event) => {
         scoreCounter.textContent = "Your final score is: ";
     } else {
         scoreCounter.textContent = "Your final score is: " + score;
-        tempFScore.push(score);
     }});
 
 // another load Event Listener designed to JSON parse the values that are put into local storage via an array below.
-// if tempPName and tempFScore are not null, the ScoreList section shows the stored information as a way of saved data.
+// if the scoreArray name and ScoreArray score are not null, the ScoreList section shows the stored information as a way of saved data.
 // The entire reason why I went through a stringify and parse process for these values was because I believed that I could
 // make several names and scores appear automatically as long as they were in Local Storage. I couldn't figure it out and could only get the last one to show.
 
 
 addEventListener('load', (event) => {
-    let tempPName = JSON.parse(localStorage.getItem("Stored-Name"));
-    let tempFScore = JSON.parse(localStorage.getItem("Stored-Final-Score"));
-    if (tempPName && tempFScore != null) {
-        scoreList.append('<li>' + tempPName + ": " + tempFScore + '</li>');
+    if (localStorage.getItem("highScores")) {
+    scoreArray = JSON.parse(localStorage.getItem("highScores"));
     } else {
-        return
-}});
+        scoreArray = []
+    }
+    console.log(scoreArray);
+    for (let i = 0; i < scoreArray.length; i++) {
+    if (scoreArray[i].name != null && scoreArray[i].score != null) {
+        scoreList.append('<li>' + scoreArray[i].name + ": " + scoreArray[i].score + '</li>');
+}}});
 
 //jQuery to declare the score-form and clear-score element ids from HTML
 
@@ -47,7 +46,7 @@ let clearFormEl = $('#clear-score');
 
 //The function that takes the name entered in the input field, sends an alert if the Enter button is pushed without anything,
 // disables the Enter button after it is pushed, so multiple inputs cannot be made, appends the data together into an unordered list,
-// pushes the playerName to the tempPName array, then takes the info in both tempPName and tempFScore and stringifies it.
+// pushes the playerName and Score to the varScore object and stringifies it.
 // Lastly, the scoreCounter is meant to not show a score after it has been input and enter is clicked.
 
 
@@ -61,9 +60,11 @@ function handleLogScore(event) {
   scoreBtn.disabled = true
   scoreList.append('<li>' + playerName + ": " + score + '</li>');
   $('input[name="input-field"]').val('');
-  tempPName.push(playerName)
-  localStorage.setItem("Stored-Name", JSON.stringify(tempPName));
-  localStorage.setItem("Stored-Final-Score", JSON.stringify(tempFScore))
+  let varScore = {
+    name: playerName, score: score
+  }
+  scoreArray.push(varScore);
+  localStorage.setItem("highScores", JSON.stringify(scoreArray));
   scoreCounter.textContent = "";
 }
 
